50f3395d25756a2469f32519cb89d3ea045f9004
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-04-07 23:11:32 +0530

    init hashtable, topology and threads

--- monitor/main.py
+++ monitor/main.py
@@ -7,20 +7,100 @@
 
 """
 
-from monitor.monitor_gossip import heartbeat_protocol
+import threading
 import socket
 import pickle
 import sys
+
 sys.path.insert('../utils/')
 from transfer import _send_msg, _recv_msg
 from object_pg import DataObject, PlacementGroup
+from monitor.monitor_gossip import heartbeat_protocol
+
+def recv_write_acks():
+    pass
+
+def recv_osd_inactive_status():
+    pass
+
+def recv_client_reqs():
+    pass
 
 def main():
-    init_hashtable
-    init_network_topology
-    recv_write_acks_thread
-    recv_req_client_thread
-    recv_osd_status_thread
+    hashtable = {
+                "pg_id1":["osd_id1", "osd_id2", "osd_id3"],
+                "pg_id2":["osd_id4", "osd_id5", "osd_id6"]
+                 }
+    hashtable_dump = pickle.dumps(hashtable)
+    hashtable_file = open('hashtable', 'wb')
+    hashtable_file.write(hashtable_dump)
+    hashtable_file.close()
+
+    cluster_topology = {
+                        "osd_id1":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":['osd_id2', 'osd_id3']
+                        },
+
+                        "osd_id2":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":['osd_id1', 'osd_id6']
+                        },
+
+                        "osd_id3":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":['osd_id1']
+                        },
+
+                        "osd_id4":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":[]
+                        },
+
+                        "osd_id5":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":[]
+                        },
+
+                        "osd_id6":
+                        {
+                            "ip":'blah',
+                            "port":1211,
+                            "free_space":100,
+                            "friends":['osd_id2']
+                        },  
+                    }
+    cluster_topology_dump = pickle.dumps(cluster_topology)
+    cluster_topology_file = open('cluster_topology', 'wb')
+    cluster_topology_file.write(cluster_topology_dump)
+    cluster_topology_file.close()
+
+    write_acks_thread = threading.Thread(target=recv_write_acks)
+    client_reqs_thread = threading.Thread(target=recv_client_reqs)
+    osd_inactive_status_thread = threading.Thread(target=recv_osd_inactive_status)
+
+    write_acks_thread.start()
+    client_reqs_thread.start()
+    osd_inactive_status_thread.start()
+
+    write_acks_thread.join()
+    client_reqs_thread.join()
+    osd_inactive_status_thread.join()
     #HEADERSIZE = 10
 
     # s = socket.socket()
