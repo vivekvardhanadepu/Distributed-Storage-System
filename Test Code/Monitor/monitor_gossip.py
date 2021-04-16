51b239bdf1fbd77235cdf7dc02df9e6aca8e404f
harshit-soora <soora.iitkgp@gmail.com>
2021-04-16 21:31:21 +0530

    This folder have all the dependency for the heartbeat and replication ... Vivek & Kartikhave a look at it .. I have debug it mostly and will update it soon .. meanwhile you guys can also have a look

---
+++ Test Code/Monitor/monitor_gossip.py
@@ -0,0 +1,99 @@
+import socket
+import pickle
+import os
+from transfer import _send_msg, _recv_msg, _wait_recv_msg
+from info import mds_ip, monitor_ip, storage_ip, num_objects_per_file, max_num_objects_per_pg, MSG_SIZE, HEADERSIZE
+
+from monitor_replicate import recovery
+
+def __init__():
+	self.live_osd = {"osd_id1":True, "osd_id2":True, "osd_id3":True, "osd_id4":True}
+
+
+
+def gossip(self, c, msg):
+	node_ip = msg["ip"]
+	crash_osd_id = msg["osd_id"]
+
+	if self.live_osd[crash_osd_id] == False:
+		return 
+	
+	print(f"Need to start the recovery protocol for {node_ip} {crash_osd_id}")
+
+	hashtable = {"pg_id1":[("osd_id1", True), ("osd_id2", True), ("osd_id4", True)]}
+	# Use this in maincode
+	# hashtable_file = open('hashtable', 'rb')
+	# hashtable_dump = hashtable_file.read()
+	# hashtable = pickle.load(hashtable_dump)
+	# hashtable_file.close()
+
+
+	self.live_osd[crash_osd_id] = False
+	rf = 3
+
+	hastable = recovery(crash_osd_id, hashtable, self.live_osd, rf)
+	print(hashtable)
+
+
+
+
+def heartbeat_protocol(soc):
+	# Establish connection with client. 
+	c, addr = soc.accept()  
+	print(f"\nGot connection from {addr}")
+	
+	n = os.fork()
+
+	if n == 0:
+		print(F"Inside child process {os.getpid()}")
+		msg = _recv_msg(c, MSG_SIZE)
+		print(msg)
+
+		if msg == None:
+			print(f"Didn't receive data! [Timeout] {addr}")
+
+		elif msg["type"] == "ALIVE":
+			res = {"type": "ACK"}
+			_send_msg(c, res)
+
+		elif msg["type"] == "FAIL":
+			res = {"type": "ACK"}
+			_send_msg(c, res)
+			gossip(c, msg)
+			
+		c.close()
+
+		print(f"Exiting from pid {os.getpid()} ..\n")
+		os._exit(1)
+
+	return
+
+
+
+if __name__ == "__main__":
+	s = socket.socket()         
+	print ("Socket successfully created")
+	  
+	# reserve a port on your computer in our 
+	port =  monitor_ip["primary"]["port"]       
+	  
+	# Next bind to the port 
+	# we have not typed any ip in the ip field 
+	# instead we have inputted an empty string 
+	# this makes the server listen to requests 
+	# coming from other computers on the network 
+	s.bind(('', port))         
+	print ("Monitor socket binded to %s" %(port)) 
+	  
+	# put the socket into listening mode 
+	s.listen(5)              
+	  
+	# a forever loop until we interrupt it or 
+	# an error occurs 
+	while True: 
+	  
+		heartbeat_protocol(s)
+		
+	s.close()
+	# Close the connection with the client 
+	#c.close()
